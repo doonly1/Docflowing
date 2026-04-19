0d089e1c33fe1803ccc9c4ca19dbc8b534809e19
doonly1 <doonly@yeah.net>
2026-04-19 22:29:40 +0800

    num

--- mystyle.py
+++ mystyle.py
@@ -156,7 +156,7 @@ def my_number_style(doc):
         try:
             s = src['page number']
             run_fm(s, '宋体', 14)
-            style_attr(s, 20 if src is doc.styles else 24)
+            style_attr(s, 20)
             done = True
             break
         except Exception:
@@ -165,4 +165,4 @@ def my_number_style(doc):
     if not done:
         add_style(doc, 'page number', 20)
         run_fm(doc.styles['page number'], '宋体', 14)
-        para_fm(doc.styles['page number'], 0, 0, 1, 16, 16, 0, 'R')
+        para_fm(doc.styles['page number'], 0, 0, 1, 14, 14, 0, 'R')
