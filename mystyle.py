6673dc5136d3dc35bbc0fdc0b607e6fec0ffa778
doonly1 <doonly@yeah.net>
2026-04-19 22:25:46 +0800

    docx num

--- mystyle.py
+++ mystyle.py
@@ -154,9 +154,9 @@ def my_number_style(doc):
     done = False
     for src in [doc.styles, doc.styles.latent_styles]:
         try:
-            s = src['page number']
-            run_fm(s, '宋体', 14)
-            style_attr(s, 20 if src is doc.styles else 24)
+            run_fm(src['page number'], '宋体', 14)
+            para_fm(src['page number'], 0, 0, 1, 14, 14, 0, 'R')
+            style_attr(src['page number'], 20)
             done = True
             break
         except Exception:
